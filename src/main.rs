a70518974bebfa24eb855607b22bd2e39dfaefef
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-12-22 07:36:59 -0200

    Receive as parameter the image tag
    Assume there's a common docker image build directory.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
 mod dockerfile;
 
 use self::dockerfile::Dockerfile;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 pub struct Arguments {
-    #[structopt(parse(from_os_str))]
-    image: PathBuf,
+    #[structopt(
+        name = "images directory",
+        short = "d",
+        long = "base-dir",
+        default_value = "/project",
+        parse(from_os_str)
+    )]
+    images_dir: PathBuf,
+    image_tag: String,
 }
 
 fn main() {
     let arguments = Arguments::from_args();
     let mut dockerfiles = Vec::new();
+    let base_dir = arguments.images_dir.join("janitovff");
 
-    let dockerfile = Dockerfile::from_file(&arguments.image).unwrap();
+    let image_name = if arguments.image_tag.starts_with("janitovff/") {
+        let (_, image_name) = arguments.image_tag.split_at(10);
+        image_name
+    } else {
+        &arguments.image_tag
+    };
+
+    let image_dir = base_dir.join(image_name);
+    let dockerfile = Dockerfile::from_file(image_dir.join("dockerfile.yml")).unwrap();
     let mut template_image = dockerfile.from().to_owned();
-    let base_dir = arguments
-        .image
-        .parent()
-        .and_then(|image_dir| image_dir.parent())
-        .unwrap_or_else(|| Path::new("."));
 
     dockerfiles.push(dockerfile);
 
