9f8abd4479c89a8ed8329e70650f032934361742
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-12-31 16:03:57 +0000

    Create `DockerRun` helper type

---
+++ src/docker/docker_run.rs
@@ -0,0 +1,62 @@
+use super::docker_command::DockerCommand;
+use std::{borrow::Cow, io};
+
+pub struct DockerRun<'a> {
+    image: Cow<'a, str>,
+    command: DockerCommand<'a>,
+}
+
+impl<'a> DockerRun<'a> {
+    pub fn new(image: impl Into<Cow<'a, str>>) -> Self {
+        let mut command = DockerCommand::new();
+        command.append("run");
+
+        DockerRun {
+            image: image.into(),
+            command,
+        }
+    }
+
+    pub fn temporary(&mut self) -> &mut Self {
+        self.command.append("--rm");
+        self
+    }
+
+    pub fn interactive(&mut self) -> &mut Self {
+        self.command.append("-it");
+        self
+    }
+
+    pub fn volume(&mut self, source: impl AsRef<str>, target: impl AsRef<str>) -> &mut Self {
+        self.command
+            .append("-v")
+            .append(format!("{}:{}", source.as_ref(), target.as_ref()));
+        self
+    }
+
+    pub fn env(&mut self, variable: impl AsRef<str>, value: impl AsRef<str>) -> &mut Self {
+        self.command
+            .append("-e")
+            .append(format!("{}={}", variable.as_ref(), value.as_ref()));
+        self
+    }
+
+    pub fn run(self) -> Result<(), io::Error> {
+        let mut command = self.command;
+
+        command.append(self.image);
+        command.run()
+    }
+
+    pub fn run_shell_command(self, shell_command: impl AsRef<str>) -> Result<(), io::Error> {
+        let mut command = self.command;
+
+        command
+            .append(self.image)
+            .append("sh")
+            .append("-c")
+            .append(shell_command.as_ref());
+
+        command.run()
+    }
+}
--- src/docker/mod.rs
+++ src/docker/mod.rs
@@ -1,1 +1,9 @@
 mod docker_command;
+mod docker_run;
+
+pub use self::docker_run::DockerRun;
+use std::borrow::Cow;
+
+pub fn run<'a>(image: impl Into<Cow<'a, str>>) -> DockerRun<'a> {
+    DockerRun::new(image)
+}
