9c5771129296a314b9c14aa28ec0be30a67dc6b6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-01-10 10:05:21 +0000

    Move commands enumeration to `commands` module

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,47 +1,17 @@
 use super::{
-    commands::{Build, Clean, New, Run, RunBuildError, RunCleanError, RunNewError, RunRunError},
+    commands::{Commands, RunCommandError},
     config::Config,
 };
-use failure::Fail;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-pub enum Arguments {
-    #[structopt(name = "build")]
-    Build(Build),
-
-    #[structopt(name = "clean")]
-    Clean(Clean),
-
-    #[structopt(name = "new")]
-    New(New),
-
-    #[structopt(name = "run")]
-    Run(Run),
-}
-
-#[derive(Debug, Fail)]
-pub enum RunCommandError {
-    #[fail(display = "Failed to build image")]
-    Build(#[cause] RunBuildError),
-
-    #[fail(display = "Failed to remove stale images")]
-    Clean(#[cause] RunCleanError),
-
-    #[fail(display = "Failed to create new project")]
-    New(#[cause] RunNewError),
-
-    #[fail(display = "Failed to run project environment")]
-    Run(#[cause] RunRunError),
+pub struct Arguments {
+    #[structopt(subcommand)]
+    command: Commands,
 }
 
 impl Arguments {
     pub fn run_command(self, config: Config) -> Result<(), RunCommandError> {
-        match self {
-            Arguments::Build(build) => build.run(config).map_err(RunCommandError::Build),
-            Arguments::Clean(clean) => clean.run().map_err(RunCommandError::Clean),
-            Arguments::New(new) => new.run().map_err(RunCommandError::New),
-            Arguments::Run(run) => run.run().map_err(RunCommandError::Run),
-        }
+        self.command.run(config)
     }
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,7 +3,53 @@ mod clean;
 mod new;
 mod run;
 
-pub use self::build::{Build, RunBuildError};
-pub use self::clean::{Clean, RunCleanError};
-pub use self::new::{New, RunNewError};
-pub use self::run::{Run, RunRunError};
+pub use self::{
+    build::{Build, RunBuildError},
+    clean::{Clean, RunCleanError},
+    new::{New, RunNewError},
+    run::{Run, RunRunError},
+};
+use super::config::Config;
+use failure::Fail;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+pub enum Commands {
+    #[structopt(name = "build")]
+    Build(Build),
+
+    #[structopt(name = "clean")]
+    Clean(Clean),
+
+    #[structopt(name = "new")]
+    New(New),
+
+    #[structopt(name = "run")]
+    Run(Run),
+}
+
+#[derive(Debug, Fail)]
+pub enum RunCommandError {
+    #[fail(display = "Failed to build image")]
+    Build(#[cause] RunBuildError),
+
+    #[fail(display = "Failed to remove stale images")]
+    Clean(#[cause] RunCleanError),
+
+    #[fail(display = "Failed to create new project")]
+    New(#[cause] RunNewError),
+
+    #[fail(display = "Failed to run project environment")]
+    Run(#[cause] RunRunError),
+}
+
+impl Commands {
+    pub fn run(self, config: Config) -> Result<(), RunCommandError> {
+        match self {
+            Commands::Build(build) => build.run(config).map_err(RunCommandError::Build),
+            Commands::Clean(clean) => clean.run().map_err(RunCommandError::Clean),
+            Commands::New(new) => new.run().map_err(RunCommandError::New),
+            Commands::Run(run) => run.run().map_err(RunCommandError::Run),
+        }
+    }
+}
